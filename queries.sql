993cd5a67e0c4a6699893896b9e7a49797b78969
thericktastic <love@The-RT-MBP.attlocal.net>
2020-02-19 17:19:30 -0500

    Answered questions in Multi-Table Queries section

--- queries.sql
+++ queries.sql
@@ -2,8 +2,40 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT Product.*,
+       Category.CategoryName
+  FROM Product
+       JOIN
+       Category ON Product.CategoryId = Category.Id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+SELECT [Order].id,
+       [Order].OrderDate,
+       [Order].ShipVia,
+       Shipper.CompanyName
+  FROM [Order]
+       JOIN
+       Shipper ON [Order].ShipVia = Shipper.id
+ WHERE [Order].OrderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+SELECT *,
+       Product.ProductName
+  FROM OrderDetail
+       JOIN
+       Product ON Product.Id = OrderDetail.ProductId
+ WHERE OrderDetail.OrderId = 10251
+ ORDER BY Product.ProductName ASC;
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+SELECT [Order].id,
+       Customer.CompanyName,
+       Employee.LastName
+  FROM [Order]
+       JOIN
+       Customer ON [Order].CustomerId = Customer.Id
+       JOIN
+       Employee ON [Order].EmployeeId = Employee.Id;
